b8e000f525e98b41bf18fed9c1741ca270d6408b
Duah Kwadwo Adjei <duah14@outlook.com>
2023-05-06 02:05:06 +0000

    Fixed CMP Tab completions

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,16 +1,53 @@
 return {
     "hrsh7th/nvim-cmp",
     opts = function()
+        local has_words_before = function()
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+
         local cmp = require("cmp")
         local luasnip = require("luasnip")
+
+        -- Set configuration for specific filetype.
+        cmp.setup.filetype("gitcommit", {
+            sources = cmp.config.sources({
+                { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+            }, {
+                { name = "buffer" },
+            }),
+        })
+
+        -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline("/", {
+            sources = {
+                { name = "buffer" },
+            },
+        })
+
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(":", {
+            sources = cmp.config.sources({
+                { name = "path" },
+            }, {
+                { name = "cmdline" },
+            }),
+        })
+
         return {
-            mapping = {
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            preselectMode = "none",
+            mapping = cmp.mapping({
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.expand_or_jumpable() then
                         luasnip.expand_or_jump()
-                    elseif cmp.has_words_before() then
+                    elseif has_words_before() then
                         cmp.complete()
                     else
                         fallback()
@@ -25,7 +62,48 @@ return {
                         fallback()
                     end
                 end, { "i", "s" }),
-            },
+                ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+                ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+                ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+                ["<C-y>"] = cmp.config.disable,
+                ["<C-e>"] = cmp.mapping({
+                    i = cmp.mapping.abort(),
+                    c = cmp.mapping.close(),
+                }),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "path" },
+                { name = "buffer" },
+            }),
         }
+        -- local cmp = require("cmp")
+        -- local luasnip = require("luasnip")
+        -- return {
+        --     mapping = {
+        --         ["<Tab>"] = cmp.mapping(function(fallback)
+        --             if cmp.visible() then
+        --                 cmp.select_next_item()
+        --             elseif luasnip.expand_or_jumpable() then
+        --                 luasnip.expand_or_jump()
+        --             elseif cmp.has_words_before() then
+        --                 cmp.complete()
+        --             else
+        --                 fallback()
+        --             end
+        --         end, { "i", "s" }),
+        --         ["<S-Tab>"] = cmp.mapping(function(fallback)
+        --             if cmp.visible() then
+        --                 cmp.select_prev_item()
+        --             elseif luasnip.jumpable(-1) then
+        --                 luasnip.jump(-1)
+        --             else
+        --                 fallback()
+        --             end
+        --         end, { "i", "s" }),
+        --     },
+        -- }
     end,
 }
