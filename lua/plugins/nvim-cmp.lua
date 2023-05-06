5cc7feeea38f68717498db083f56a8597cfbcbb6
Duah Kwadwo Adjei <duah14@outlook.com>
2023-05-06 02:01:47 +0000

    Fixed CMP Tab completions

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,31 @@
+return {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        return {
+            mapping = {
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif cmp.has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            },
+        }
+    end,
+}
